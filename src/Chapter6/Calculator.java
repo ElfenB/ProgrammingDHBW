5348a0c1b7b615ad423e1e9050befa6a585adb17
ElfenB <56521054+ElfenB@users.noreply.github.com>
2019-10-24 21:50:37 +0200

    added parity testing to calculator

--- src/Chapter6/Calculator.java
+++ src/Chapter6/Calculator.java
@@ -8,7 +8,30 @@ public class Calculator {
 	}
 	
 	public static void main(String[] args) {
+		int actionNumber;
 		Scanner scan = new Scanner(System.in);
+		
+		// input
+		System.out.println("Bitte Aktion auswählen:");
+		System.out.println("1. Paritätstest");
+		actionNumber = scan.nextInt();
+		switch (actionNumber) {
+		case 1:	
+			System.out.println("Bitte ganzzahlige Zahl für Paritätstest eingeben.");
+			int input = scan.nextInt();
+			if (parity(input)) {
+				System.out.println("Die eingegebene Zahl " + input + " ist gerade.");
+			} else {
+				System.out.println("Die eingegebene Zahl " + input + " ist ungerade.");
+			}
+			
+			break;
+
+		default:
+			break;
+		}
+		
+		// output
 		
 		scan.close();
 	}
