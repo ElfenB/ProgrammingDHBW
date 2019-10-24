0469313451fbeb8da9faa5273d94848a0755af6c
ElfenB <56521054+ElfenB@users.noreply.github.com>
2019-10-24 21:42:28 +0200

    added scanner and parity to calculator

--- src/Chapter6/Calculator.java
+++ src/Chapter6/Calculator.java
@@ -1,9 +1,16 @@
 package Chapter6;
+import java.util.Scanner;
 
 public class Calculator {
 
+	public static boolean parity(int a) {
+		return (a % 2 == 0 ? true : false);
+	}
+	
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 		
+		scan.close();
 	}
 
 }
