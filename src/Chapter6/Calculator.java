aceff500056448f700a04e2aa6b2ececbbb8cf66
ElfenB <56521054+ElfenB@users.noreply.github.com>
2019-10-24 22:23:32 +0200

    added averages

--- src/Chapter6/Calculator.java
+++ src/Chapter6/Calculator.java
@@ -23,6 +23,18 @@ public class Calculator {
 		}
 	}
 	
+	public static double arithmeticAverage(int a, int b) {
+		return (a + b) / 2;
+	}
+	
+	public static double geometricAverage(int a, int b) {
+		return Math.sqrt(a * b);
+	}
+	
+	public static double harmonicAverage(int a, int b) {
+		return 2.0 / ((1.0 / a) + (1.0 / b));
+	}
+	
 	public static void main(String[] args) {
 		int actionNumber;
 		Scanner scan = new Scanner(System.in);
@@ -30,19 +42,48 @@ public class Calculator {
 		// input
 		System.out.println("Bitte Aktion auswählen:");
 		System.out.println("1. Paritätstest");
+		System.out.println("2. Betrag");
+		System.out.println("3. Mittelwert zweier Zahlen");
 		actionNumber = scan.nextInt();
 		switch (actionNumber) {
 		case 1:	
 			System.out.println("Bitte ganzzahlige Zahl für Paritätstest eingeben.");
-			int input = scan.nextInt();
-			if (parity(input)) {
-				System.out.println("Die eingegebene Zahl " + input + " ist gerade.");
+			int parityInput = scan.nextInt();
+			if (parity(parityInput)) {
+				System.out.println("Die eingegebene Zahl " + parityInput + " ist gerade.");
 			} else {
-				System.out.println("Die eingegebene Zahl " + input + " ist ungerade.");
+				System.out.println("Die eingegebene Zahl " + parityInput + " ist ungerade.");
 			}
-			
 			break;
-
+		case 2: 
+			System.out.println("Bitte Zahl eingeben, um den Betrag zu berechnen.");
+			double absInput = scan.nextDouble();
+			System.out.println("Der Betrag der eingegeben Zahl " + absInput + " ist " + abs(absInput) + ".");
+			break;
+		case 3:
+			int a, b;
+			System.out.println("Bitte Zahl 1 eingeben:");
+			a = scan.nextInt();
+			System.out.println("Bitte Zahl 2 eingeben:");
+			b = scan.nextInt();
+			System.out.println("Bitte wählen:");
+			System.out.println("1. Arithmetisches Mittel");
+			System.out.println("2. Geometrisches Mittel");
+			System.out.println("3. Harmonisches Mittel");
+			switch (scan.nextInt()) {
+			case 1:
+				System.out.println("Das arithmetisches Mittel der Zahlen " + a + " und " + b + " ist " + arithmeticAverage(a, b) + ".");
+				break;
+			case 2:
+				System.out.println("Das geometrisches Mittel der Zahlen " + a + " und " + b + " ist " + geometricAverage(a, b) + ".");
+				break;
+			case 3:
+				System.out.println("Das harmonisches Mittel der Zahlen " + a + " und " + b + " ist " + harmonicAverage(a, b) + ".");
+				break;
+			default:
+				break;
+			}
+			break;
 		default:
 			break;
 		}
