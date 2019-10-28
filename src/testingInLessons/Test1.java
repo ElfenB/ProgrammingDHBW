a6f5adbdbe3f0cdbd64a916b961e87e042b6a067
ElfenB <56521054+ElfenB@users.noreply.github.com>
2019-10-28 13:36:33 +0100

    testing var

--- src/testingInLessons/Test1.java
+++ src/testingInLessons/Test1.java
@@ -1,5 +1,11 @@
 package testingInLessons;
 
 public class Test1 {
-
+	public static void main(String[] args) {
+		var test = 3;
+		var nochEinTest = 4;
+		var ausgabe = "Hallo ";
+		
+		System.out.println(ausgabe + (test + nochEinTest));
+	}
 }
