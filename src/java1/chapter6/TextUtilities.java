d855c8e437a775135981fa3d2a2df409652b3a10
ElfenB <56521054+ElfenB@users.noreply.github.com>
2019-10-30 21:40:58 +0100

    new class with text utilities

---
+++ src/java1/chapter6/TextUtilities.java
@@ -0,0 +1,22 @@
+package java1.chapter6;
+
+import java.util.Scanner;
+
+public class TextUtilities {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		switch (scan.nextLine()) {
+		case "1":
+			
+			break;
+
+		default:
+			break;
+		}
+		
+		scan.close();
+	}
+
+}
