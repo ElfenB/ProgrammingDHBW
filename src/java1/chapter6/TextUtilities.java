f5c1e0c79bf5de4f0886a3f5e4a3398a9087a3d0
ElfenB <56521054+ElfenB@users.noreply.github.com>
2019-10-30 22:48:06 +0100

    added showLongestWord method

--- src/java1/chapter6/TextUtilities.java
+++ src/java1/chapter6/TextUtilities.java
@@ -272,6 +272,65 @@ public class TextUtilities {
 		System.out.println("Der eingegebene Text \"" + input + "\" wurde verschlüsselt zu: \"" + caesarChiffre(input, chiffreKey) + "\".");
 	}
 	
+	public static void showLongestWords(String input) {
+		String scannedWord = "";
+		int highestLength = 0;
+		String longestWords = "";
+		int wordLength = 0;
+		boolean multipleWords = false;
+		
+		// TODO weniger abgefuckte Lösung finden als das Folgende?
+		// delete spaces at beginning and end
+		input.trim();
+		// B1G TR1CK !!! Add space on input to run for-loop one time longer to cover the last word, too
+		input += " ";
+		
+		for (int i = 0; i < input.length(); i++) {
+			if (input.charAt(i) != ' ') {
+				// go through input String and put the single words in scannedWords
+				scannedWord += input.charAt(i);
+				// increase word length
+				wordLength++;
+			} else {
+				if (wordLength > highestLength) {
+					// if the scanned word is longer than the current stored longest word  it's the new one
+					longestWords = scannedWord;
+					// set new highest length
+					highestLength = wordLength;
+					// relevant for output line
+					multipleWords = false;
+				} else if (wordLength == highestLength) {
+					// add new word behind current if same length
+					longestWords += ", " + scannedWord;
+					// relevant for output line
+					multipleWords = true;
+				} else {
+					
+				}
+				// reset values for next scanned word
+				wordLength = 0;
+				scannedWord = "";
+			}
+		}
+		
+		// output
+		System.out.println("");
+		// use the boolean to create friendly user-experience
+		if (multipleWords) {
+			System.out.println("Die längsten Worte in diesem Text waren:");
+		} else {
+			System.out.println("Das längste Wort in diesem Text war:");
+		}
+		System.out.println(longestWords + " mit einer Länge von " + highestLength + ".");
+	}
+	
+	private static void showLongestWordInstructions(Scanner scan) {
+		// input
+		System.out.println("Bitte Text eingeben, um das längste Wort/die längsten Wörter finden zu können.");
+		String input = scan.nextLine();
+		showLongestWords(input);
+	}
+	
 	/**
 	 * This generates the beautiful menu that's displayed on startup
 	 */
@@ -312,7 +371,12 @@ public class TextUtilities {
 			Calculator.terminateProgram();
 			finished = true;
 			break;
-//		case "5":
+		case "5":
+			showLongestWordInstructions(scan);
+			Calculator.terminateProgram();
+			finished = true;
+			break;
+//		case "6":
 //			
 //			Calculator.terminateProgram();
 //			finished = true;
@@ -323,6 +387,7 @@ public class TextUtilities {
 			break;
 		}
 	}
+
 	
 	public static boolean finished = false;
 
