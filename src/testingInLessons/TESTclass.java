79cb5d4a6d71e78d4be06c0448ea08b9ea5d2de5
ElfenB <56521054+ElfenB@users.noreply.github.com>
2019-10-31 14:16:23 +0100

    tested something

--- src/testingInLessons/TESTclass.java
+++ src/testingInLessons/TESTclass.java
@@ -1,11 +1,16 @@
 package testingInLessons;
 
+import java.util.Scanner;
+
 public class TESTclass {
 	public static void main(String[] args) {
-		long sum = 0;
-		for (long i = 0; i < 9999999; i++) {
-			sum =+ i;
-			System.out.println(sum);
-		}
+//		long sum = 0;
+//		for (long i = 0; i < 9999999; i++) {
+//			sum =+ i;
+//			System.out.println(sum);
+//		}
+		Scanner scan = new Scanner(System.in);
+		System.out.println(scan.nextInt());
+		scan.close();
 	}
 }
