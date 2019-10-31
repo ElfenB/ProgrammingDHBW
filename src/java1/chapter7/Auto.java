99ab0e724b4b3ed38f227ab87587734f3e56540d
ElfenB <56521054+ElfenB@users.noreply.github.com>
2019-10-31 16:30:35 +0100

    changed name and added constructor

---
+++ src/java1/chapter7/Auto.java
@@ -0,0 +1,20 @@
+package java1.chapter7;
+
+
+public class Auto {
+	public static int autoAnzahl;
+	public String kennzeichen;
+	public String modell;
+	public int insassenAnzahl;
+	public int tankInhalt;
+	
+	public Auto(String kennzeichen, String modell, int insassenAnzahl, int tankInhalt) {
+		this.kennzeichen = kennzeichen;
+		this.modell = modell;
+		this.insassenAnzahl = insassenAnzahl;
+		this.tankInhalt = tankInhalt;
+	}
+
+	
+	
+}
--- src/java1/chapter7/Automobile.java
+++
@@ -1,5 +0,0 @@
-package java1.chapter7;
-
-public class Automobile {
-	
-}
