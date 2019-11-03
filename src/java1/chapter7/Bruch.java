10f39b6995ce68c0cf2a6fd89c4883c4d3693637
ElfenB <56521054+ElfenB@users.noreply.github.com>
2019-11-03 15:29:59 +0100

    added comments and adding method

--- src/java1/chapter7/Bruch.java
+++ src/java1/chapter7/Bruch.java
@@ -1,11 +1,12 @@
 package java1.chapter7;
+import java1.chapter6.Calculator;
 
 /**
  * @author Benjamin Elfen
  * 
  * This class helps calculating with fractions
  * <br />
- * <img height="80%" src="http://yuml.me/diagram/scruffy/class/[;Bruch%7C+zaehler:%20long;+nenner:%20long%7C+Bruch(zaehler:%20long%E2%80%9A%20nenner:%20long);+negiere():%20Bruch;+addiere(summand:%20Bruch):%20Bruch;+subtrahiere(subtrahend:%20Bruch):%20Bruch;+kehrwert():%20Bruch;+multipliziere(faktor:%20Bruch):%20Bruch;+dividiere(divisor:%20Bruch):%20Bruch;+potenziere(exponent:%20int):%20Bruch;+istGleich(bruch:%20Bruch):%20boolean;+toDouble():%20double;+toString():%20String]">
+ * <img src="http://yuml.me/diagram/scruffy/class/[;Bruch%7C+zaehler:%20long;+nenner:%20long%7C+Bruch(zaehler:%20long%E2%80%9A%20nenner:%20long);+negiere():%20Bruch;+addiere(summand:%20Bruch):%20Bruch;+subtrahiere(subtrahend:%20Bruch):%20Bruch;+kehrwert():%20Bruch;+multipliziere(faktor:%20Bruch):%20Bruch;+dividiere(divisor:%20Bruch):%20Bruch;+potenziere(exponent:%20int):%20Bruch;+istGleich(bruch:%20Bruch):%20boolean;+toDouble():%20double;+toString():%20String]">
  *
  */
 public class Bruch {
@@ -14,12 +15,32 @@ public class Bruch {
 	public final long nenner;
 	
 	// constructor
+	/**
+	 * @param zaehler is the value of the upper part of a fraction
+	 * @param nenner is the value of the lower part of a fraction
+	 */
 	public Bruch(long zaehler, long nenner) {
 		// assign given values to final longs
+		if (nenner < 0) {
+			zaehler *= -1;
+		}
+//		Calculator.ggT(zaehler, nenner);
 		this.zaehler = zaehler;
 		this.nenner = nenner;
 	}
 	
+	// can invert the fraction (multiply with -1)
+	public Bruch negiere() {
+		Bruch output = new Bruch(zaehler * -1, nenner);
+		return output;
+	}
 	
+	// can give out the value of the fraction (format: zaehler/nenner ; example: 1/2)
+	public String toString() {
+		return (zaehler + "/" + nenner);
+	}
 	
+	public Bruch addiere(Bruch input) {
+		return new Bruch(zaehler * input.nenner + input.zaehler * nenner, nenner * input.nenner);
+	}
 }
---
+++ src/java1/chapter7/BruchBenutzen.java
@@ -0,0 +1,15 @@
+package java1.chapter7;
+
+public class BruchBenutzen {
+	public static void main(String[] args) {
+		Bruch bruch1 = new Bruch(1, 2);
+		System.out.println("Zähler: " + bruch1.zaehler);
+		System.out.println("Nenner: " + bruch1.nenner);
+		System.out.println("<======================>");
+		System.out.println("Negieren");
+		System.out.println(bruch1.negiere().toString());
+		System.out.println("<======================>");
+		Bruch bruch2 = new Bruch(3, 4);
+		System.out.println(bruch1.addiere(bruch2).toString());
+	}
+}
