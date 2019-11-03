64907b82d1e8a0677d100c5c93a32a985cedcd59
ElfenB <56521054+ElfenB@users.noreply.github.com>
2019-11-03 14:33:18 +0100

    added comments

--- src/java1/chapter7/Auto.java
+++ src/java1/chapter7/Auto.java
@@ -2,17 +2,25 @@ package java1.chapter7;
 
 
 public class Auto {
+	// static variable for counting sum of cars
 	public static int autoAnzahl;
+	// Strings for number plate & model
 	public String kennzeichen;
 	public String modell;
+	// integers for number of people in the car and fuel in the tank
 	public int insassenAnzahl;
 	public int tankInhalt;
 	
+	// constructor for adding new car
 	public Auto(String kennzeichen, String modell) {
+		// assigning values to variables
 		this.kennzeichen = kennzeichen;
 		this.modell = modell;
+		// count up the amount of cars
+		autoAnzahl++;
 	}
 
+	// methods for actions with the car
 	public int einsteigen(int personen) {
 		insassenAnzahl += personen;
 		return insassenAnzahl;
@@ -22,7 +30,6 @@ public class Auto {
 		insassenAnzahl -= personen;
 		return insassenAnzahl;
 	}
-	
 	
 	public int tanken(int liter) {
 		tankInhalt += liter;
