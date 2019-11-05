be8ef5f3328d4d0e1214084f05a1e31d597c1159
ElfenB <56521054+ElfenB@users.noreply.github.com>
2019-11-05 21:32:29 +0100

    added tests

--- src/java1/chapter8/ContainerTestPlausibel.java
+++ src/java1/chapter8/ContainerTestPlausibel.java
@@ -8,19 +8,19 @@ public class ContainerTestPlausibel {
 		int containerGroesse = (int) (67.7 * 1000); 	// m³ in liter (dm³)
 		
 		Container Haushalt = new Container(containerZuladung40feet, containerGroesse);
-		Transportgut Wandschrank = new Transportgut(200, 6.4);
+		Transportgut Wandschrank = new Transportgut(200, (0.8 * 2 * 4) * 1000); 	// depth * height * width => liters
 		Transportgut BigToy = new Transportgut(28000, 80000);
 		
-//		Haushalt.beladen(Wandschrank, true);
-//		System.out.println();
-//		Haushalt.beladen(BigToy, true);
+		Haushalt.beladen(Wandschrank, true);
+		System.out.println();
+		Haushalt.beladen(BigToy, true);
 		
 		int i = 0;
 		while (Haushalt.beladen(Wandschrank, false)) {
 			Haushalt.beladen(Wandschrank, true);
 			i++;
 		}
-		System.out.println("Wandschränke geladen: " + i);
+		System.out.println("Es konnten noch " + i + " Wandschränke geladen werden.");
 	}
 	
 	
