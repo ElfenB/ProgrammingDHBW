180006d28659ef567fbfc5df21b6198d599f1e89
ElfenB <56521054+ElfenB@users.noreply.github.com>
2019-11-07 16:37:29 +0100

    worked on pet class and tested

--- src/java1/chapter9/Haustier.java
+++ src/java1/chapter9/Haustier.java
@@ -1,5 +1,26 @@
 package java1.chapter9;
 
 public class Haustier {
-	
+	private String name;
+    private int anzahlBeine;
+
+    protected Haustier(String name, int anzahlBeine) {
+        super();                                        // optional
+        this.name = name;
+        this.anzahlBeine = anzahlBeine;
+    }
+
+    public String getName() {
+    	return this.name;
+    }
+    
+    public int getAnzahlBeine() {
+    	return this.anzahlBeine;
+    }
+    
+    public String toString() {
+    	return "Dieses Tier hat " + getAnzahlBeine() + " Beine und heißt " + getName() + ".";
+    }
+    
+    // TODO main-Methode mit einfachem Test
 }
---
+++ src/java1/chapter9/HaustierTest.java
@@ -0,0 +1,10 @@
+package java1.chapter9;
+
+public class HaustierTest {
+
+	public static void main(String[] args) {
+		Haustier Hund = new Haustier("Bernd", 4);
+		System.out.println(Hund);
+	}
+
+}
