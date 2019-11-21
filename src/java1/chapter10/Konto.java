bb619b0cdb7826cae45de07fafa197d3f46c28ca
Benjamin <Benjamin@BenjaminMain.fritz.box>
2019-11-21 16:51:20 +0100

    added new class of excercise 1

---
+++ src/java1/chapter10/Konto.java
@@ -0,0 +1,67 @@
+package java1.chapter10;
+/**
+ * 
+ * @author Benjamin Elfen
+ * 
+ * Ein Konto mit Kontonummer und Saldo, das Ein- und Auszahlungen unterstützt.
+ * 
+ */
+public class Konto {
+    private final long nummer;      // Kontonummer
+    private double saldo = 0;       // aktueller Saldo
+
+    /**
+     * Konto-Konstruktor.
+     * 
+     * @param nummer Kontonummer
+     */
+    public Konto(long nummer) {
+        super();                    // optional
+        this.nummer = nummer;
+    }
+
+    /**
+     * Liefert die Kontonummer.
+     * 
+     * @return Kontonummer
+     */
+    public long getNummer() {
+        return nummer;
+    }
+
+    /**
+     * Liefert den aktuellen Saldo des Kontos.
+     * 
+     * @return Saldo
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * Erhöht den Saldo um den genannten Betrag.
+     * 
+     * @param betrag negative Werte werden ignoriert
+     */
+    public void einzahlen(double betrag) {
+        if (betrag > 0) {
+            saldo += betrag;
+        }
+    }
+
+    /**
+     * Vermindert den Saldo um den genannten Betrag.
+     * 
+     * @param betrag negative Werte werden ignoriert
+     */
+    public void auszahlen(double betrag) {
+        if (betrag > 0) {
+            saldo -= betrag;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return nummer + ": " + saldo + " Euro";
+    }
+}
