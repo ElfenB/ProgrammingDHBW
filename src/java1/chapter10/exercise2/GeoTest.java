1c64ca502dfce408b9a12164c7a307306f81b082
Benjamin <Benjamin@BenjaminMain.fritz.box>
2019-11-23 16:04:25 +0100

    added testing class

---
+++ src/java1/chapter10/exercise2/GeoTest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package java1.chapter10.exercise2;
+
+/**
+ * @author Benjamin Elfen
+ *
+ */
+public class GeoTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
