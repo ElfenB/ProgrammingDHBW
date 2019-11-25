0a7981616ca2dee3ada520c36b0e3ab09428be62
ElfenB <56521054+ElfenB@users.noreply.github.com>
2019-11-25 15:00:42 +0100

    made code easier

--- src/testingInLessons/Array.java
+++ src/testingInLessons/Array.java
@@ -11,11 +11,13 @@ public class Array {
 		zahlen[4] = 8;
 		zahlen[2] = 9;
 		
+		System.out.println(java.util.Arrays.toString(zahlen));
 		java.util.Arrays.sort(zahlen, 0, zahlen.length);
+		System.out.println(java.util.Arrays.toString(zahlen));
 		
-		for (int i = 0; i < zahlen.length; i++) {
-			System.out.println(zahlen[i]);
-		}
+//		for (int i = 0; i < zahlen.length; i++) {
+//			System.out.println(zahlen[i]);
+//		}
 		
 		String[] personen = new String[4];
 		personen[3] = "Gerda";
@@ -23,11 +25,14 @@ public class Array {
 		personen[2] = "Wuffi";
 		personen[0] = "Werner";
 		
+		System.out.println(java.util.Arrays.toString(personen));
 		java.util.Arrays.sort(personen);
+		System.out.println(java.util.Arrays.toString(personen));
 		
-		for (int i = 0; i < personen.length; i++) {
-			System.out.println(personen[i]);
-		}
+//		for (int i = 0; i < personen.length; i++) {
+//			System.out.println(personen[i]);
+//		}
+		
 		
 	}
 }
