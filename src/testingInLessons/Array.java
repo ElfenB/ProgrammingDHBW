f2bc2b30fd1c4f5414a0de8bfa58d31a067d2826
ElfenB <56521054+ElfenB@users.noreply.github.com>
2019-11-25 14:29:57 +0100

    testing class for arrays sorting

---
+++ src/testingInLessons/Array.java
@@ -0,0 +1,33 @@
+package testingInLessons;
+
+public class Array {
+	public static void main(String[] args) {
+		int[] zahlen = new int[6];
+		for (int i = 0; i < zahlen.length; i++) {
+			zahlen[i] = i;
+		}
+		
+		zahlen[3] = 8;
+		zahlen[4] = 8;
+		zahlen[2] = 9;
+		
+		java.util.Arrays.sort(zahlen, 0, zahlen.length);
+		
+		for (int i = 0; i < zahlen.length; i++) {
+			System.out.println(zahlen[i]);
+		}
+		
+		String[] personen = new String[4];
+		personen[3] = "Gerda";
+		personen[1] = "Herbert";
+		personen[2] = "Wuffi";
+		personen[0] = "Werner";
+		
+		java.util.Arrays.sort(personen);
+		
+		for (int i = 0; i < personen.length; i++) {
+			System.out.println(personen[i]);
+		}
+		
+	}
+}
