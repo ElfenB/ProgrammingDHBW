988100ea9424010344e96a709ac5457954602da4
ElfenB <56521054+ElfenB@users.noreply.github.com>
2019-11-26 18:10:03 +0100

    added comments and testing

--- src/java1/chapter11/FibonacciIterative.java
+++ src/java1/chapter11/FibonacciIterative.java
@@ -1,4 +1,5 @@
 package java1.chapter11;
+
 public class FibonacciIterative {
     public static long fibonacci(int n) {
     	// array for fibonacci calculation (n is biggest summand)
@@ -12,6 +13,22 @@ public class FibonacciIterative {
 //    	System.out.println(java.util.Arrays.toString(fib));
     	return fib[fib.length - 1];
     }
+    
+    private static long[] fibRecursive = new long[100];
+    
+    public static long fibonacciRecursive(int n) {
+    	// abortion condition1
+    	if (n == 1) {
+    		fibRecursive[n] = 1;
+    	// abortion condition 2
+		} else if (n == 0) {
+			fibRecursive[n] = 0;
+		// add up numbers in the array
+		} else {
+	        fibRecursive[n] = n + fibonacciRecursive(n - 1);
+		}
+    	return fibRecursive[n];
+    }
 
     public static void main(String[] args) {
     	// test output
@@ -20,5 +37,22 @@ public class FibonacciIterative {
     	System.out.println("Fibonacci-Zahl von 3: " + fibonacci(3));
     	System.out.println("Fibonacci-Zahl von 90: " + fibonacci(90));
     	System.out.println("Fibonacci-Zahl von 92: " + fibonacci(92));
+
+    	// test output for recursive calculation
+    	System.out.println("Fibonacci-Zahl von 0: " + fibonacciRecursive(0) + " (rekursiv)");
+    	System.out.println("Fibonacci-Zahl von 1: " + fibonacciRecursive(1) + " (rekursiv)");
+    	System.out.println("Fibonacci-Zahl von 3: " + fibonacciRecursive(3) + " (rekursiv)");
+    	System.out.println("Fibonacci-Zahl von 90: " + fibonacciRecursive(90) + " (rekursiv)");
+    	System.out.println("Fibonacci-Zahl von 92: " + fibonacciRecursive(92) + " (rekursiv)");
+    	
+    	if (fibonacci(0) == fibonacciRecursive(0) & 
+    		fibonacci(1) == fibonacciRecursive(1) & 
+    		fibonacci(3) == fibonacciRecursive(3) & 
+    		fibonacci(90) == fibonacciRecursive(90) &
+    		fibonacci(92) == fibonacciRecursive(92)) {
+			System.out.println("Entweder geht alles oder beides ist gleich falsch.");
+		} else {
+			System.out.println("Irgendwas läuft hier gehörig falsch.");
+		}
     }
 }
