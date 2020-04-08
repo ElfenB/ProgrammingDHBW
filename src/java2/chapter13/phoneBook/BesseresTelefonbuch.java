79e539ed622bcae0213ff4966badd9a735b4c838
Benjamin <Benjamin@BenjaminMain.fritz.box>
2020-04-08 17:32:33 +0200

    updates some things

--- src/java2/chapter13/phoneBook/BesseresTelefonbuch.java
+++ src/java2/chapter13/phoneBook/BesseresTelefonbuch.java
@@ -1,3 +1,6 @@
+/**
+ * 
+ */
 package java2.chapter13.phoneBook;
 
 import java.util.Arrays;
@@ -5,26 +8,26 @@ import java.util.Arrays;
 /**
  * @author Benjamin Elfen
  * 
- * This Phone Book V1.0 can handle simple entries that can be searched linear (demonstration of linear search)
+ * This is V1.1 of the Telefonbuch class (demonstration of binary search)
  *
  */
-public class Telefonbuch {
+public class BesseresTelefonbuch {
 	private int entries = 0;
-	private Telefonbucheintrag[] Telefonbuch = new Telefonbucheintrag[100];
+	private Telefonbucheintrag[] BesseresTelefonbuch = new Telefonbucheintrag[100];
 		
 	//adds new entries to phone book
 	public void add(Telefonbucheintrag eintrag) {
-		if (entries == Telefonbuch.length) {
+		if (entries == BesseresTelefonbuch.length) {
             throw new IllegalStateException("Telefonbuch ist schon voll.");
         }
-		Telefonbuch[entries++] = eintrag;
+		BesseresTelefonbuch[entries++] = eintrag;
 	}
 	
 	// gives ability to perform a linear search
 	public Telefonbucheintrag search(String name) {
 		for (int i = 0; i < this.getNumberOfEntries() - 1; i++) {
-			if (Telefonbuch[i].getName() == name) {
-				return Telefonbuch[i];
+			if (BesseresTelefonbuch[i].getName() == name) {
+				return BesseresTelefonbuch[i];
 			}
 		}
 		return new Telefonbucheintrag(name, "nicht im Telefonbuch gefunden");
@@ -36,7 +39,6 @@ public class Telefonbuch {
 	
 	@Override
 	public String toString() {
-		return Arrays.toString(this.Telefonbuch);
+		return Arrays.toString(this.BesseresTelefonbuch);
 	}
-	
 }
--- src/java2/chapter13/phoneBook/TestingClassPhoneBook.java
+++ src/java2/chapter13/phoneBook/TestingClassPhoneBook.java
@@ -5,6 +5,8 @@ package java2.chapter13.phoneBook;
 
 /**
  * @author Benjamin Elfen
+ * 
+ * This class is for testing the Telefonbuch class
  *
  */
 public class TestingClassPhoneBook {
@@ -13,7 +15,16 @@ public class TestingClassPhoneBook {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		new Telefonbuch()
+		// Testing
+		Telefonbuch GelbeSeiten = new Telefonbuch();
+		
+		GelbeSeiten.add(new Telefonbucheintrag("Kunibert", "015243665487"));
+		GelbeSeiten.add(new Telefonbucheintrag("Gerd", "080034646"));
+		
+		System.out.println(GelbeSeiten);
+		
+		System.out.println(GelbeSeiten.search("Kunibert"));
+		System.out.println(GelbeSeiten.search("Gert"));
 	}
 
 }
