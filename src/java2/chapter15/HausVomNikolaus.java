7242a610a62fbf9839a1eae78ffb238a89e60706
Benjamin <Benjamin@BenjaminMain.fritz.box>
2020-05-06 22:23:21 +0200

    building a house works

--- src/java2/chapter15/HausVomNikolaus.java
+++ src/java2/chapter15/HausVomNikolaus.java
@@ -13,7 +13,7 @@ public class HausVomNikolaus {
 
 	private static int[] punkte = new int[5];
 	private static int[] striche = new int[8];
-	private static int target = 0;
+	private static int cursor = 0;
 	
 	private static final int[][] wegenetz = new int[][] {
 //		   0
@@ -32,14 +32,19 @@ public class HausVomNikolaus {
 	};
 	
 	private static final int[][] punkteToWege = new int[][] {
-		{ 0, 1 }, // der Weg von 0 nach 1 ist 0, der Weg von 0 nach 2 ist 1 (bezieht sich auf wegenetz)
-		{ 0, 2, 3, 4 }, // 1 -> 0 = 0, ...
-		{ 1, 2, 5, 6 }, 
-		{ 3, 5, 7 }, 
-		{ 4, 6, 7 }
+//		  0/\1
+//		  /  \
+//		 2———— 
+//		3|4\/5|6 	// links oben nach rechts unten = 4
+//		 | /\ |
+//		  ————7
+		// 0, 1, 2, 3, 4
+		{ -1, 0, 1, -1, -1 }, // 0 	// der Weg von 0 nach 1 ist 0, der Weg von 0 nach 2 ist 1 (bezieht sich auf wegenetz)
+		{ 0, -1, 2, 3, 4 },   // 1	// 1 -> 0 = 0, ...
+		{ 1, 2, -1, 5, 6 },   // 2
+		{ -1, 3, 5, -1, 7 },  // 3
+		{ -1, 4, 6, 7, -1 }   // 4
 	};
-	
-	private static int startpunkt = 3;
 	
 	private static void arraysZuruecksetzen() {
 		for (int i = 0; i < punkte.length; i++) {
@@ -50,136 +55,142 @@ public class HausVomNikolaus {
 		}
 	}
 	
+	/**
+	 * @param start der Startpunkt (z.B. unten links)
+	 * @param verbose regelt die Ausgabe der Zwischenbauten und Variablenausgaben
+	 */
 	private static void zugMachen(int start, boolean verbose) {
-		if (verbose) {
-			printOutHouse();
-		}
+		if (verbose) printOutHouse();
 		
 		// TODO Logik
-		if (striche[punkteToWege[start][target]] < 1) {
-			striche[punkteToWege[start][target]]++;
-			
-		}
-		zugMachen(target, verbose);
 		
-//		for (int i = 0; i < wegenetz[start].length; i++) {
-//			if (striche[punkteToWege[start][i]] < 1) {
-//				striche[i]++;
-//				target = i++;
-//			}
-//		}
+		int target = wegenetz[start][cursor];
+		
+		if (striche[punkteToWege[start][target]] == 0) {
+			if (verbose) System.out.println("start" + start + " target" + target + " " + punkteToWege[start][target] + "=0");
+			striche[punkteToWege[start][target]]++;
+			cursor = 0;
+			zugMachen(target, verbose);
+		} else if (striche[punkteToWege[start][target]] == 1) {
+			if (ergebnisErreicht()) return;
+			if (verbose) System.out.println("start" + start + " target" + target + " " + punkteToWege[start][target] + "=1");
+			cursor++;
+			zugMachen(start, verbose);
+		}
 		
 		
 	}
 	
+	/**
+	 * @return sagt aus ob das Haus fertig gebaut ist
+	 */
 	private static boolean ergebnisErreicht() {
-		
 		for (int i = 0; i < striche.length; i++) {
 			if (striche[i] == 0) {
 				return false;
-//			} else if (striche[i] > 1) {
-//				throw new IllegalArgumentException("Der Strich wurde schon besucht");
 			}
 		}
-		
 		return true;
 	}
 	
+	/**
+	 * Well, it prints out the house - relying on striche[]
+	 */
 	private static void printOutHouse() {
 		System.out.println(Arrays.toString(striche));
 		String output = "";
-		// Fährt über die Ausgabe linienweise 6 ist hardcoded für 6 Linien
+		// Fährt über die Ausgabe linienweise, 6 ist hardcoded für 6 Linien (Höhe)
 		for (int i = 0; i < 6; i++) {
 			switch (i) {
 			case 0: // Erste Linie der Ausgabe
-				if (striche[1] == 1) {
+				if (striche[0] == 1) {
 					output += "  /";
-				} else if (striche[1] == 0) {
+				} else if (striche[0] == 0) {
 					output += "   ";
 				}
-				if (striche[2] == 1) {
+				if (striche[1] == 1) {
 					output += "\\  ";
-				} else if (striche[2] == 0) {
+				} else if (striche[1] == 0) {
 					output += "   ";
 				}
 				output += "\n"; // neue Zeile
 				
 				break;
 			case 1:
-				if (striche[1] == 1) {
+				if (striche[0] == 1) {
 					output += " / ";
-				} else if (striche[1] == 0) {
+				} else if (striche[0] == 0) {
 					output += "   ";
 				}
-				if (striche[2] == 1) {
+				if (striche[1] == 1) {
 					output += " \\ ";
-				} else if (striche[2] == 0) {
+				} else if (striche[1] == 0) {
 					output += "   ";
 				}
 				output += "\n"; // neue Zeile
 				
 				break;
 			case 2:
-				if (striche[6] == 1) {
+				if (striche[2] == 1) {
 					output += " ————";
-				} else if (striche[1] == 0) {
+				} else if (striche[2] == 0) {
 					output += "   ";
 				}
 				output += "\n"; // neue Zeile
 				
 				break;
 			case 3:
-				if (striche[0] == 1) {
+				if (striche[3] == 1) {
 					output += "| ";
-				} else if (striche[1] == 0) {
+				} else if (striche[3] == 0) {
 					output += "  ";
 				}
-				if (striche[7] == 1) {
+				if (striche[4] == 1) {
 					output += "\\";
-				} else if (striche[2] == 0) {
+				} else if (striche[4] == 0) {
 					output += " ";
 				}
 				if (striche[5] == 1) {
 					output += "/ ";
-				} else if (striche[2] == 0) {
+				} else if (striche[5] == 0) {
 					output += "  ";
 				}
-				if (striche[3] == 1) {
+				if (striche[6] == 1) {
 					output += "|";
-				} else if (striche[2] == 0) {
+				} else if (striche[6] == 0) {
 					output += " ";
 				}
 				output += "\n"; // neue Zeile
 				
 				break;
 			case 4:
-				if (striche[0] == 1) {
+				if (striche[3] == 1) {
 					output += "| ";
-				} else if (striche[1] == 0) {
+				} else if (striche[3] == 0) {
 					output += "  ";
-				}
-				if (striche[7] == 1) {
-					output += "/";
-				} else if (striche[2] == 0) {
-					output += " ";
 				}
 				if (striche[5] == 1) {
+					output += "/";
+				} else if (striche[5] == 0) {
+					output += " ";
+				}
+				if (striche[4] == 1) {
 					output += "\\ ";
-				} else if (striche[2] == 0) {
+				} else if (striche[4] == 0) {
 					output += "  ";
 				}
-				if (striche[3] == 1) {
+				if (striche[6] == 1) {
 					output += "|";
-				} else if (striche[2] == 0) {
+				} else if (striche[6] == 0) {
 					output += " ";
 				}
 				output += "\n"; // neue Zeile
 				
 				break;
 			case 5:
-				if (striche[4] == 1) {
+				if (striche[7] == 1) {
 					output += " ————";
-				} else if (striche[2] == 0) {
+				} else if (striche[7] == 0) {
 					output += "";
 				}
 				output += "\n"; // neue Zeile
@@ -215,25 +226,10 @@ public class HausVomNikolaus {
 		arraysZuruecksetzen();
 		
 		// Startpunkt festlegen
-		startpunkt = 3;		// unten links
-		
-//		   0
-//		   /\
-//		  /  \
-//		1 ———— 2
-//		 | \/ |
-//		 | /\ |
-//		3 ———— 4
+		int startpunkt = 3;		// unten links
 		
 		
 		zugMachen(startpunkt, true);
-		
-		printOutHouse();
-		
-		for (int i = 0; i < wegenetz.length; i++) {
-			System.out.println(Arrays.toString(wegenetz[i]));
-		}
-		
 		
 		
 		
